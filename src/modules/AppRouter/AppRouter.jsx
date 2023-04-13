554b6e021b46e419e23487863da50a68104594a4
sergpav2007 <sergpav2007@gmail.com>
2023-04-13 12:58:54 +0300

    Internship-fe-3-2

--- src/modules/AppRouter/AppRouter.jsx
+++ src/modules/AppRouter/AppRouter.jsx
@@ -5,22 +5,17 @@ import * as constants from '../../constants/constants';
 
 const AppRouter = () => {
     const user = true;
-
     return (
         user ?
             <Routes>
-                {routes.privateRoutes.map(({ path, Component }) =>
-                    <Route key={path} path={path} component={() => <Component />} exact />
-                )}
+                {routes.privateRoutes.map(({ path, Component }) => <Route key={path} path={path} component={() => <Component />} exact />)}
                 <Link to={constants.MAIN_ROUTE}/>
             </Routes>
-        :
-        <Routes>
-            {routes.publicRoutes.map(({ path, Component }) => 
-                <Route key={path} path={path} component={() => <Component />} exact />
-            )}
-            <Link to={constants.LOGIN_ROUTE}/>
-        </Routes>
+            :
+            <Routes>
+                {routes.publicRoutes.map(({ path, Component }) => <Route key={path} path={path} component={() => <Component />} exact />)}
+                <Link to={constants.LOGIN_ROUTE}/>
+            </Routes>
     )
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import createSagaMiddleware from 'redux-saga';
-import { applyMiddleware } from 'redux';
+// import { applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
+// import { getDefaultMiddleware } from '@reduxjs/toolkit';
 
 export const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [composeWithDevTools(applyMiddleware(sagaMiddleware))]
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
+  // middleware: [composeWithDevTools(applyMiddleware(sagaMiddleware))],
 });
 
 export default store;
